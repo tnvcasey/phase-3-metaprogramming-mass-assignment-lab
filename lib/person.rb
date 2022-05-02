525aa6ca4ec91a4d37e63e607012bc660ee4f63f
taylorvercillo <taylornvcasey@gmail.com>
2022-05-02 14:18:08 -0600

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,14 @@
+require 'pry'
+
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  
 
   def initialize(attributes)
     attributes.each do |key, value|
+      self.class.attr_accessor(key)
       self.send("#{key}=", value)
     end
   end
 end
+
+
